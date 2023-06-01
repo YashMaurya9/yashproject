8a22d76b550ad243d70c29e40283a83b7e74a6c0
YashMaurya9 <yasharyanmaurya0484753@gmail.com>
2023-06-01 18:26:02 +0530

    even-odd

---
+++ problem1.js
@@ -0,0 +1,11 @@
+// if i want to check a Numberis even or odd;
+
+// if the reminder is 0 we can say is even;
+
+let Number = 2;
+if(Number%2==0){
+    console.log("Even Number");
+}
+else{
+    console.log("Odd Number");
+}
